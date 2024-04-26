561f5231a3c4f491ac0a593db3eae1c01aff708c
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-04-26 22:01:07 +0530

    Version 01

--- Glyph_ai/ai/main.py
+++ Glyph_ai/ai/main.py
@@ -19,12 +19,24 @@ input_text=st.text_input("How can i help you")
 #Prompt Template
 
 first_input_prompt=PromptTemplate(
-    input_variables=['Word '],
-    template="tell me about  {Word}"
+    input_variables=['Prompt '],
+    template="tell me about  {Prompt}"
 
 )
-llm=OpenAI(temperature=0.8)
+#openai integration
+llm=OpenAI(temperature=0.1)
 chain=LLMChain(llm=llm,prompt=first_input_prompt,verbose=True,output_key='Description')
 
+#second prompt template
+second_input_prompt=PromptTemplate(
+    input_variables=['Description'],
+    template="What is the {Description} of the following text"
+
+)
+# chain 
+chain2=LLMChain(llm=llm,prompt=second_input_prompt,verbose=True,output_key='important_events')
+parent_chain=SimpleSequentialChain(chains=[chain,chain2],verbose=True)
+
+#output
 if input_text:
-    st.write(chain.run(input_text))
+    st.write(parent_chain.run(input_text))
