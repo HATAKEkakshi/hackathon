8b36fa556068f1dfcf2f0c4f7c1358fcd5ce4965
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-04-26 19:25:31 +0530

    Version 1.0

--- Glyph_ai/ai/main.py
+++ Glyph_ai/ai/main.py
@@ -12,3 +12,19 @@ from langchain.chains import SequentialChain
 import streamlit as st
 os.environ["OPENAI_API_KEY"]=openai_key
 
+
+st.title("Glyph")
+input_text=st.text_input("How can i help you")
+
+#Prompt Template
+
+first_input_prompt=PromptTemplate(
+    input_variables=['Word '],
+    template="tell me about  {Word}"
+
+)
+llm=OpenAI(temperature=0.8)
+chain=LLMChain(llm=llm,prompt=first_input_prompt,verbose=True,output_key='Description')
+
+if input_text:
+    st.write(chain.run(input_text))
